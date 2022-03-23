6cdae09bc2305ed4e376f19b11826720bf6f16dc
Dev Agrawal <devagrawal09@gmail.com>
2022-03-23 15:40:15 -0400

    removed page number

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,11 @@
 import React from "react";
 
-const Footer = ({ back, next, page }) => {
+const Footer = ({ back, next }) => {
   return (
     <div className="footer">
       <div className="btn" onClick={back}>
         <h3 className="btn__text">Back</h3>
       </div>
-      <div className="footer__page">Page {page + 1}</div>
       <div className="btn" onClick={next}>
         <h3 className="btn__text">Next</h3>
       </div>

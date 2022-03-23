d948626113f221fbbdd8eca7bba7ef125a8ed0b7
Dev Agrawal <devagrawal09@gmail.com>
2022-03-23 14:39:50 -0400

    bareboned

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,48 +1,27 @@
 import React, { useState, useEffect } from "react";
 import PokemonCard from "./PokemonCard";
-import PokemonPopup from "./PokemonPopup";
 import Header from "./Header";
 import Footer from "./Footer";
 import { getPokemons } from "../services/pokemons";
 
-const pageLimit = 9;
-
 const Pokedex = () => {
   const [pokemons, setPokemons] = useState([]);
-  const [selectedPokemon, selectPokemon] = useState(null);
-  const [page, setPage] = useState(0);
 
   useEffect(() => {
-    getPokemons(page * pageLimit, pageLimit).then((data) => {
+    getPokemons(0, 10).then((data) => {
       setPokemons(data.results);
     });
-  }, [page]);
-
-  const next = () => {
-    setPage((prevPage) => prevPage + 1);
-  };
-
-  const back = () => {
-    setPage((prevPage) => (prevPage ? prevPage - 1 : 0));
-  };
+  }, []);
 
   return (
     <>
       <Header />
       <div className="cards">
         {pokemons?.map((pokemon, index) => (
-          <PokemonCard
-            key={index}
-            name={pokemon.name}
-            onClick={() => selectPokemon(pokemon)}
-          />
+          <PokemonCard key={index} name={pokemon.name} />
         ))}
       </div>
-      <PokemonPopup
-        selectedPokemon={selectedPokemon}
-        close={() => selectPokemon(null)}
-      />
-      <Footer back={back} next={next} page={page} />
+      <Footer />
     </>
   );
 };
